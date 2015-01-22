f920826f4fc42cc7143c834e19606e42c431491c
Dan Stoppelman <dstop75@gmail.com>
2015-01-22 17:58:19 -0500

    done!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122224927) do
+ActiveRecord::Schema.define(version: 20150122223812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20150122224927) do
     t.text     "city"
     t.text     "state"
     t.text     "flavor"
-    t.float    "price"
   end
 
 end
